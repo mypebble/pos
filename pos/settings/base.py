175f13a282a9c367b50b1bbc7b389abdd162079b
will <will.emmerson@talktopebble.co.uk>
2016-02-04 10:09:40 +0000

    add static root

--- pos/settings/base.py
+++ pos/settings/base.py
@@ -105,6 +105,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'collected-static')
 
 try:
     from .local_settings import *
