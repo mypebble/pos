3cafe1bc59bd54e3b6769a59279052cbf251f35d
will <will.emmerson@talktopebble.co.uk>
2016-02-10 12:22:02 +0000

    clean up urls to avoid deprecation message

--- pos/urls.py
+++ pos/urls.py
@@ -13,56 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
-from django.contrib import admin
 
-from core import views
-
-from django.conf import settings
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 from pos.core.views.auth import LogoutView
 
-urlpatterns = patterns(
-    'django.contrib.auth.views',
-    url(
-        r'^login/$', 'login', name='login'),
-    url(
-        r'^logout/$', LogoutView.as_view(), {'next_page': '/'}, name='logout'),
-    url(
-        r'^password/reset/$', 'password_reset',
-        {'from_email': settings.SUPPORT_EMAIL},
-        name='password-reset'),
-    url(
-        r'^password/reset/done/$', 'password_reset_done',
-        name='password_reset_done'),
-    url(
-        (
-            r'^password/(?P<uidb64>[0-9A-Za-z]{1,13})-'
-            r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
-        ),
-        'password_reset_confirm',
-        name='set-password'),
-    url(
-        (
-            r'^reset/(?P<uidb64>[0-9A-Za-z]{1,13})-'
-            r'(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'
-        ),
-        'password_reset_confirm',
-        name='password_reset_confirm'),
-
-    url(r'^reset/done/$',
-        'password_reset_complete',
-        name='password_reset_complete'),
-
-    url(r'social/',
-        include('social.apps.django_app.urls', namespace='social')),
-)
-
-
-
-urlpatterns += [
+urlpatterns = [
+    url(r'^logout/$', LogoutView.as_view(), {'next_page': '/'}, name='logout'),
+    url(r'social/', include('social.apps.django_app.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
-#    url(r'^', views.PosView.as_view(), name='pos'),
 ]
